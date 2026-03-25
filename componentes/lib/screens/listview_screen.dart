20a3a124a7626fe061526b1268186f1f348ef21e
ElvisAMB <pegazuz@live.com>
2026-03-24 21:02:38 -0500

    Configuración de listas

--- componentes/lib/screens/listview_screen.dart
+++ componentes/lib/screens/listview_screen.dart
@@ -7,21 +7,39 @@ class ListViewScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Configuración de listas"),
+        title: const Text("Manejo de listas"),
         centerTitle: true,
         backgroundColor: Colors.lightBlue,
         //leading: Icon(Icons.arrow_back_rounded),
       ),
-      body: Column(
-        children: [
-          ListTile(
-            title: Text("ListView"),
-            subtitle: Text("Mostrar como se construyen las listas"),
-            //leading: Icon(Icons.account_tree_outlined),
-            trailing: Icon(Icons.chevron_right),
-            
-          ),
-        ],
+
+      // body: ListView(
+      //   children: [
+      //     Image.asset("assets/image.png", height: 400,),
+      //     Image.asset("assets/image.png"),
+      //     Image.asset("images/programador.jpg"),
+      //     Image.asset("assets/image.png"),
+      //     Image.asset("assets/image.png"),
+      //     Image.asset("assets/image.png"),
+      //     Image.asset("assets/image.png"),
+      //     Image.asset("assets/image.png"),
+      //   ],
+      // )
+      body: ListView.separated(
+        itemCount: 20,
+        separatorBuilder: (context, index) {
+          return Divider();
+        },
+        itemBuilder: (context, index) {
+          //Para crear un mismo diseño, se crea una lista infinita
+          //return Image.asset("assets/image.png");
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Elemento: $index"),
+            ),
+          );
+        },
       ),
     );
   }
